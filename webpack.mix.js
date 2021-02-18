f0df8526b928c7dd43bc3f2ff743c906f99b3183
Iago Mussel <him_mussel@live.com>
2021-02-18 13:09:51 -0300

    Atualizando o laravel mix para 6.0.11

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,4 +3,4 @@ const mix = require('laravel-mix');
 
 mix.sass('resources/sass/app.scss','public/assets/css')
 
-.js('resources/assets/js/app.js' , 'public/assets/js');
+.js('resources/assets/js/app.js' , 'public/assets/js').vue();
